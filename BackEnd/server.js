b365b30fd91a359af8127c77638f83c61e5dbf23
Yimmen <142782219+dasdaadas@users.noreply.github.com>
2025-05-07 16:42:27 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 app.use(cors({
-  origin:'https://fastfetch-frontend.netlify.app',
+  origin:'fast-fetch-38stfu2ky-yimmenneikes-projects.vercel.app',
   methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ['Content-Type'],
   credentials: true
