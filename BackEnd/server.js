a88d77ea686ad3dc5309fa41233a46dc812fc803
Yimmen <142782219+dasdaadas@users.noreply.github.com>
2025-05-07 15:14:45 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 app.use(cors({
-  origin:'https://fastfetch-frontend.onrender.com',
+  origin:'https://fastfetch-frontend.netlify.app',
   methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ['Content-Type'],
   credentials: true
