7b8116153f2aa49de11da7fd6b147d132cee989a
Yimmen <142782219+dasdaadas@users.noreply.github.com>
2025-05-10 01:09:34 +0530

    Update resetpassword.vue

--- FrontEnd/src/views/resetpassword.vue
+++ FrontEnd/src/views/resetpassword.vue
@@ -34,7 +34,7 @@ const tokenFromQuery = route.query.resettoken;
            
          try{
              
-             const res = await fetch(`${apiBackend}/api/resetPassword`,{
+             const res = await fetch('/api/resetPassword',{
                method: 'PUT',
                headers: {
                 "Content-Type": "application/json",
